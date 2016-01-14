0e147c2513ddbb76a24218f2ae4ef07a8c9f37a0
anisan1995 <anisan1995@gmail.com>
2016-01-14 10:06:40 -0500

    Cambios Anita 1

---
+++ ProyectoSBDD1/src/proyectosbdd1/ProyectoSBDD1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosbdd1;
+
+/**
+ *
+ * @author Anita
+ */
+public class ProyectoSBDD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
